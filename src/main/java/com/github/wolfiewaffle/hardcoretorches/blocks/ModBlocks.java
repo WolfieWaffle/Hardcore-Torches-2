8d7a1dffda6d574879943546de13a027d2473424
WolfieWaffle <wolfie.waffle11@gmail.com>
2016-05-20 11:35:06 -0600

    Comment out more errors

--- src/main/java/com/github/wolfiewaffle/hardcoretorches/blocks/ModBlocks.java
+++ src/main/java/com/github/wolfiewaffle/hardcoretorches/blocks/ModBlocks.java
@@ -1,9 +1,5 @@
 package com.github.wolfiewaffle.hardcoretorches.blocks;
 
-import com.github.wolfiewaffle.hardcoretorches.items.ItemTorchCokeLit;
-import com.github.wolfiewaffle.hardcoretorches.items.ItemTorchLit;
-import com.github.wolfiewaffle.hardcoretorches.items.ItemTorchUnlit;
-
 import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -16,10 +12,10 @@ public final class ModBlocks {
 	//public static Block test_block;
 
     public static void createBlocks() {
-    	GameRegistry.registerBlock(torch_burnt = new BlockTorchBurnt("torch_burnt"));
-    	GameRegistry.registerBlock(torch_lit = new BlockTorchLit("torch_lit"), ItemTorchLit.class);
-    	GameRegistry.registerBlock(torch_coke_lit = new BlockTorchCokeLit("torch_coke_lit"), ItemTorchCokeLit.class);
-    	GameRegistry.registerBlock(torch_unlit = new BlockTorchUnlit("torch_unlit"), ItemTorchUnlit.class);
+    	GameRegistry.register(torch_burnt = new BlockTorchBurnt("torch_burnt"));
+    	//GameRegistry.register(torch_lit = new BlockTorchLit("torch_lit"), ItemTorchLit.class);
+    	//GameRegistry.register(torch_coke_lit = new BlockTorchCokeLit("torch_coke_lit"), ItemTorchCokeLit.class);
+    	//GameRegistry.register(torch_unlit = new BlockTorchUnlit("torch_unlit"), ItemTorchUnlit.class);
     	//GameRegistry.registerBlock(torch_coke_unlit = new BlockTorchCokeUnlit("torch_coke_unlit"), ItemTorchCokeUnlit.class);
     	//GameRegistry.registerBlock(test_block = new BlockTest("test_block"));
     }
